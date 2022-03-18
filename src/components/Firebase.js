19efe9743ffe02ceba5f579f054f8de229884a78
IgboPharaoh <solomoneze62@gmail.com>
2022-03-18 06:22:35 +0100

    initial react setup

---
+++ src/components/Firebase.js
@@ -0,0 +1,13 @@
+
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBXNuePxZ4LXEsyX_AQa4uLGl8PC0thudw",
+    authDomain: "tiktok-clone-9edba.firebaseapp.com",
+    projectId: "tiktok-clone-9edba",
+    storageBucket: "tiktok-clone-9edba.appspot.com",
+    messagingSenderId: "1082900799067",
+    appId: "1:1082900799067:web:8f026b5e2031d6efaa297e",
+    measurementId: "G-EFN0CT46T8"
+  };
---
+++ src/components/Video.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './video.css'
+
+const Video = () => {
+  return (
+    <div className="video">
+        <video className="video" src="">
+        </video>
+    </div>
+  )
+}
+
+export default Video
