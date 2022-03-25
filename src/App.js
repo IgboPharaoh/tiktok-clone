29398e6854add3c9091207fe57fc9eafb4400841
IgboPharaoh <solomoneze62@gmail.com>
2022-03-25 18:41:49 +0100

    setup firebase dynamically

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
 import Video from './components/video/Video';
+import React, {useState, useEffect} from 'react'
+import db from "./Firebase";
+import 'firebase/compat/firestore';
 
 function App() {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() =>{
+    db.collection('videos').onSnapshot(snapshot => {
+      setVideos(snapshot.docs.map(doc => doc.data()))
+    })
+  }, [videos])
   return (
     <div className="App">
       <div className="app__videos">
-        <Video
-          url
-          channel
-          description
-          song
-          likes
-          messages
-          shares
-        />
-        <Video/>
-        <Video/>
-        <Video/>
+        {
+          videos.map(({url, channel, description, song, likes, messages, shares, index}) =>
+            
+            <Video
+              key={index}
+              url={url}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+            )
+        }
       </div>
     </div>
   );
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,5 @@
-
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBXNuePxZ4LXEsyX_AQa4uLGl8PC0thudw",
@@ -9,3 +10,8 @@ const firebaseConfig = {
     appId: "1:1082900799067:web:8f026b5e2031d6efaa297e",
     measurementId: "G-EFN0CT46T8"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore();
+export default db;
